af0c6780e8274f4e2e72906d37049e67817b14c3
Roberto Fortes <robertbernd23@gmail.com>
2021-12-15 21:34:01 -0300

    feat: loading information from async storage and implementing search bar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,15 +30,26 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+
+    const response = await AsyncStorage.getItem(dataKey)
+    const parsedData = JSON.parse(response);
+
+    setSearchListData(parsedData);
+    setData(parsedData);
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    const filteredData = searchListData.filter(data => {
+      if(data.service_name.includes(searchText)){
+        return data;
+      }
+
+      setSearchListData(filteredData);
+    })
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    setSearchText(text)
   }
 
   useFocusEffect(useCallback(() => {
