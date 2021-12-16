18c968c5ead9ebe24f5d5188fe043b11e5b6ab4a
Roberto Fortes <robertbernd23@gmail.com>
2021-12-15 21:47:29 -0300

    feat: adding new passwords

--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -50,6 +50,21 @@ export function RegisterLoginData() {
     const dataKey = '@savepass:logins';
 
     // Save data on AsyncStorage and navigate to 'Home' screen
+
+    const response = await AsyncStorage.getItem(dataKey);
+    const parsedData = JSON.parse(response) || [];
+
+    const newLoginListData = [
+      ...parsedData,
+      newLoginData
+    ];
+
+    await AsyncStorage.setItem(
+      dataKey, 
+      JSON.stringify(newLoginListData)
+    );
+
+    navigate('Home');
   }
 
   return (
@@ -66,8 +81,7 @@ export function RegisterLoginData() {
             title="Nome do serviço"
             name="service_name"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.service_name && errors.service_name.message
             }
             control={control}
             autoCapitalize="sentences"
@@ -78,8 +92,7 @@ export function RegisterLoginData() {
             title="E-mail"
             name="email"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.email && errors.email.message
             }
             control={control}
             autoCorrect={false}
@@ -91,8 +104,7 @@ export function RegisterLoginData() {
             title="Senha"
             name="password"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.password && errors.password.message
             }
             control={control}
             secureTextEntry
